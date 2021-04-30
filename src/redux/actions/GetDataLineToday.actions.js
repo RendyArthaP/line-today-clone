02d994cf1d8a3b28ef4e90fcc751eb6eff95549c
Rendy Artha <rendyartha312@gmail.com>
2021-04-30 10:55:02 +0700

    setup redux, actions, environment

---
+++ src/redux/actions/GetDataLineToday.actions.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+export const GET_DATA_LINETODAY_REQUEST = 'GET_DATA_LINETODAY_REQUEST';
+export const GET_DATA_LINETODAY_SUCCESS = 'GET_DATA_LINETODAY_SUCCESS';
+export const GET_DATA_LINETODAY_ERROR = 'GET_DATA_LINETODAY_ERROR';
+
+export const getDataLineTodayRequest = () => {
+  return {
+    type: GET_DATA_LINETODAY_REQUEST
+  }
+}
+
+export const getDataLineTodaySuccess = (result) => {
+  return {
+    type: GET_DATA_LINETODAY_SUCCESS,
+    result
+  }
+}
+
+export const getDataLineTodayError = (error) => {
+  return {
+    type: GET_DATA_LINETODAY_ERROR,
+    error
+  }
+}
+
+export const getDataLineToday = () => {
+  return function(dispatch) {
+    dispatch(getDataLineTodayRequest())
+
+    axios
+      .get(process.env.REACT_APP_GET_LINETODAY)
+      .then((result) => dispatch(getDataLineTodaySuccess(result.data)))
+      .catch((error) => dispatch(getDataLineTodayError(error)))
+  }
+}
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import handleGetDataLineToday from './GetDataLineToday.reducers';
+
+const rootReducers = combineReducers({
+  handleGetDataLineToday
+})
+
+export default rootReducers;
